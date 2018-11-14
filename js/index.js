426e883c6ad549e5bdc731b8069522e02398328c
Alessandro Curcu <aleqrq@gmail.com>
2018-11-14 16:13:13 +0100

    Aggiunge menù iniziale e costituzione

--- js/index.js
+++ js/index.js
@@ -10,3 +10,29 @@ const wrapper = document.getElementById('wrapper');
 const formHookButton = document.getElementById('form_hook_button');
 
 console.log(formHookButton);
+
+const dropdownMenus = document.querySelectorAll('.dropdownMenu');
+const primaryNavigationItems = document.querySelectorAll(
+  '.primaryNavigation__item'
+);
+
+console.log(primaryNavigationItems);
+
+const toggleClick = e => {
+  if (typeof e.currentTarget.children[1] === 'undefined') return;
+  e.preventDefault();
+  e.currentTarget.children[1].classList.toggle('toggle');
+};
+
+const toggleHover = e => {
+  if (typeof e.currentTarget.children[1] === 'undefined') return;
+  // e.preventDefault();
+  e.currentTarget.children[1].classList.toggle('toggle');
+};
+
+primaryNavigationItems.forEach(el => {
+  // el.addEventListener('click', toggle);
+  el.addEventListener('mouseenter', toggleHover);
+  el.addEventListener('mouseleave', toggleHover);
+  // el.addEventListener('click', toggle);
+});
